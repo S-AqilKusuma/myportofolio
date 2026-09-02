b9530d0ee07518cfe29983ec2ca92c2d0007af7e
S-AqilKusuma <sayyid.aqil@ui.ac.id>
2026-09-02 16:42:33 +0700

    fix: Removing backslash from URL.

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "localhost", 
     "127.0.0.1",
-    "sayyid-aqil-myportofolio.pws.cs.ui.ac.id/"
+    "sayyid-aqil-myportofolio.pws.cs.ui.ac.id"
 ]
 
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,6 +144,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
 
 
 # Email
