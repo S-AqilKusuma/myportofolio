8220940c39d5b287ee42dd1aa0d1f13be8647e76
S-AqilKusuma <sayyid.aqil@ui.ac.id>
2026-09-08 10:43:25 +0700

    feat: adding experience.html, modifying style.css, and connecting both templates to views.py

--- main/views.py
+++ main/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Sayyid Aqil Kusuma",
+        "npm": "2506596331",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            """A 3rd semester CS student at Universitas Indonesia. Aspiring to be a game developer.
+            Planning to graduate with minimal GPA of 3.00 within 4 years of study period (or less)."""
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Sayyid Aqil Kusuma",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
