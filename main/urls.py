7d71b5364b6f8ffe0f31a95ea5ff11accd217ee2
S-AqilKusuma <sayyid.aqil@ui.ac.id>
2026-09-15 11:30:55 +0700

    feat: implementing data delivery with JSON, adding experience search, and adding experience delete

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_skill, create_experience
+from main.views import (
+    show_main,
+    show_experience,
+    show_skill,
+    create_experience, 
+    get_experience_json,
+    delete_experience
+)
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
+    path("api/experience/", get_experience_json, name="get_experience_json"),
     path("experience/", show_experience, name="show_experience"),
     path("experience/add/", create_experience, name="create_experience"),
+    path("experience/<uuid:experience_id>/delete/",delete_experience,name="delete_experience"),
     path("skill/", show_skill, name="show_skill"),
 ]
