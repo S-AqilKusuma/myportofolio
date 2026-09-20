b9b7b35775231f42e392e8d560841882d06712ac
S-AqilKusuma <sayyid.aqil@ui.ac.id>
2026-09-20 07:43:01 +0700

    feat: adding skill API, create, and delete URL

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 from main.views import (
     show_main,
     show_experience,
-    show_skill,
     create_experience, 
     get_experience_json,
-    delete_experience
+    delete_experience,
+    show_skill,
+    create_skill,
+    get_skill_json,
+    delete_skill
 )
 
 app_name = "main"
@@ -14,8 +17,11 @@ app_name = "main"
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("api/experience/", get_experience_json, name="get_experience_json"),
+    path("api/skill/", get_skill_json, name="get_skill_json"),
     path("experience/", show_experience, name="show_experience"),
     path("experience/add/", create_experience, name="create_experience"),
     path("experience/<uuid:experience_id>/delete/",delete_experience,name="delete_experience"),
     path("skill/", show_skill, name="show_skill"),
+    path("skill/add/", create_skill, name="create_skill"),
+    path("skill/<uuid:skill_id>/delete/", delete_skill ,name="delete_skill"),
 ]
